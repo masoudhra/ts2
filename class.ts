6770cba05b649b876275f0aa358e0c1239eb5362
Masoud Ramezani <masoudhra@gmail.com>
2024-01-25 14:08:46 +1300

    comment some code in class file

--- class.ts
+++ class.ts
@@ -1,49 +1,49 @@
 // Class -> User
 // backend -> create data & read DB send to frontend
 // front -> read & send data from/to backend 
-type profile = {
-    name: string;
-    mobile: string;
-    age: number;
-}
-type EditProfile = {
-    name?: string | undefined;
-    mobile?: string | undefined;
-    age?: number | undefined;
-}
-class User{
-    private name: string;
-    protected mobile: string;
-    private age: number;
-    constructor(name: string, mobile: string, age: number){
-        this.name = name;
-        this.mobile = mobile;
-        this.age = age
-    }
-    getUserProfile(): profile{
-        return {
-            name: this.name,
-            mobile: this.mobile,
-            age: this.age
-        }
-    }
-    editUserProfile(profileOBJ : EditProfile): EditProfile{
-        if(profileOBJ.age){
-            this.age = profileOBJ.age
-        }
-        if(profileOBJ.name){
-            this.name = profileOBJ.name
-        }
-        if(profileOBJ.mobile){
-            this.mobile = profileOBJ.mobile
-        }
-        return {
-            name: this.name,
-            mobile: this.mobile,
-            age: this.age
-        }
-    }
-}
-const user : User = new User("Masoud Ramezani", "0936485435", 33)
-user.editUserProfile({age: 35})
-console.log(user.getUserProfile());
+// type profile = {
+//     name: string;
+//     mobile: string;
+//     age: number;
+// }
+// type EditProfile = {
+//     name?: string | undefined;
+//     mobile?: string | undefined;
+//     age?: number | undefined;
+// }
+// class User{
+//     private name: string;
+//     protected mobile: string;
+//     private age: number;
+//     constructor(name: string, mobile: string, age: number){
+//         this.name = name;
+//         this.mobile = mobile;
+//         this.age = age
+//     }
+//     getUserProfile(): profile{
+//         return {
+//             name: this.name,
+//             mobile: this.mobile,
+//             age: this.age
+//         }
+//     }
+//     editUserProfile(profileOBJ : EditProfile): EditProfile{
+//         if(profileOBJ.age){
+//             this.age = profileOBJ.age
+//         }
+//         if(profileOBJ.name){
+//             this.name = profileOBJ.name
+//         }
+//         if(profileOBJ.mobile){
+//             this.mobile = profileOBJ.mobile
+//         }
+//         return {
+//             name: this.name,
+//             mobile: this.mobile,
+//             age: this.age
+//         }
+//     }
+// }
+// const user : User = new User("Masoud Ramezani", "0936485435", 33)
+// user.editUserProfile({age: 35})
+// console.log(user.getUserProfile());
